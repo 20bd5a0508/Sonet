3ec8c7d2cf2dbe6b0c92f4a02324fefa4cb6eb95
Arun_Kumar <20bd5a0508@gmail.com>
2021-05-12 19:47:19 +0530

    second commit

--- app/src/main/java/com/example/ktdemo1/Main.kt
+++ app/src/main/java/com/example/ktdemo1/Main.kt
@@ -1,5 +1,19 @@
 package com.example.ktdemo1
 
 fun main (){
-    print("hello")
+   /* var myInt=45L
+    var myByte:Byte=25
+    var myBoolean:Boolean=true
+    var myName:String="Ravaan"
+    var firstChar=myName[0]
+    var lastChar=myName[myName.length-1]
+    var myArray= arrayOf(1,2,3,"numbers",'a','b',false)
+    print("hello $myBoolean"+"\t"+myInt+myByte+"\n"+firstChar+"\t"+lastChar+"\n"+"${myArray[4]}")*/
+    var myString:String="Android Kotlin"
+    var myFloat:Float=13.37f
+    val myDouble:Double=3.14159265358979
+    var myInt:Byte=25
+ //   val pi=22/7
+    print("$myString\n$myFloat\n$myDouble\n$myInt")
+    //print("%5f".format(pi))
 }
--- app/src/main/java/com/example/ktdemo1/MainActivity.kt
+++ app/src/main/java/com/example/ktdemo1/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.ktdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,17 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var btn=findViewById<Button>(R.id.btn)
-        var txt=findViewById<TextView>(R.id.txt)
+        var txt1=findViewById<TextView>(R.id.txt1)
         var flag=false
+        var ptxt1=findViewById<EditText>(R.id.pt1)
         btn.setOnClickListener(View.OnClickListener {
-            if(flag){
-                txt.setText("hello kotlin")
-                flag=false;
-            }
-            else{
-                txt.setText("hello world")
-                flag=true
-            }
+            var radius=ptxt1.text.toString().toDouble()
+            val pi=3.14285
+            var area =pi*radius*radius
+            txt1.setText("Area of circle:$area")
+
+
+
+
         })
     }
 }
