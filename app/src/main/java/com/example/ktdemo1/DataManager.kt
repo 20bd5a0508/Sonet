e4eb69dd945ff533c6dac4c0c3f80c27abee9e5b
Arun_Kumar <20bd5a0508@gmail.com>
2021-06-23 20:37:50 +0530

    note day2

--- app/src/main/java/com/example/ktdemo1/DataManager.kt
+++ app/src/main/java/com/example/ktdemo1/DataManager.kt
@@ -5,6 +5,7 @@ class DataManager {
     var notes=ArrayList<NoteInfo>()
     init {
         initCourse()
+        initNote()
     }
 
     private fun initCourse(){
@@ -14,5 +15,14 @@ class DataManager {
         courses.put(course.courseId,course)
         course= Courseclass("ML_2021","Machine Learning with Python")
         courses.put(course.courseId,course)
+    }
+    private fun initNote(){
+        var note=NoteInfo(courses["Kotlin_2021"]  as Courseclass,"Activity","Learning Andriod Activity")
+        notes.add(note)
+
+        note=NoteInfo(courses["Kotlin_2021"]  as Courseclass,"Intent","Learning Andriod Intent ")
+        notes.add(note)
+
+
     }
 }
--- app/src/main/java/com/example/ktdemo1/NoteInfo.kt
+++ app/src/main/java/com/example/ktdemo1/NoteInfo.kt
@@ -1,5 +1,7 @@
 package com.example.ktdemo1
 
 class NoteInfo(var course: Courseclass,var title:String,var text:String) {
-
+    override fun toString(): String {
+        return "${course.title} -$title"
+    }
 }
